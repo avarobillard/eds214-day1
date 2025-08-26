b169f9f5db477ceec7070f4cc4afcec57628371b
avarobillard <avamorganr@gmail.com>
2025-08-26 15:53:36 -0700

    some tidying

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# eds214-day1
+---
+title: "README"
+output: html_document
+---
+
+# Reproduction of a figure from Schaefer et al. (2000)
+
+## Repository purpose
+
+This repository contains code to reproduce Figure 3, which shows the effects of hurricane disturbance on stream water concentrations in a subset of four tropical forest watersheds of the Luquillo Experimental Forest, Puerto Rico.
+
+## Repository structure
+
+-   Data: data
+-   Analysis script:
+-   Supporting code:
+-   Outputs: figs
+
+## Data access
+
+Raw data can be accessed from the Environmental Data Initiative (EDI) Data Portal, in a package titled "Chemistry of stream water from Luquillo Mountains" (see <https://doi.org/10.6073/PASTA/F31349BEBDC304F758718F4798D25458>).
+
+Download the following .csv files:
+
+-   Quebrada one-Bisley (Q1) Chemistry Data
+
+-   Quebrada two-Bisley (Q2) Chemistry Data
+
+-   Quebrada three-Bisley (Q3) Chemistry Data
+
+-   Puente Roto Mameyes (MPR) Chemistry Data
+
+## Contributors
+
+Ava Robillard
+
+## References
+
+McDowell, William H., and USDA Forest Service. International Institute Of Tropical Forestry (IITF). 2024. “Chemistry of Stream Water from the Luquillo Mountains.” Environmental Data Initiative. <https://doi.org/10.6073/PASTA/F31349BEBDC304F758718F4798D25458>.
+
+Schaefer, Douglas. A., William H. McDowell, Fredrick N. Scatena, and Clyde E. Asbury. 2000. “Effects of Hurricane Disturbance on Stream Water Concentrations and Fluxes in Eight Tropical Forest Watersheds of the Luquillo Experimental Forest, Puerto Rico.” Journal of Tropical Ecology 16 (2): 189–207. <https://doi.org/10.1017/s0266467400001358>.
--- scratch/spaghetti.R
+++ scratch/spaghetti.R
@@ -1,62 +1,118 @@
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                               Script purpose                             ----
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Read in raw data, clean data, conduct analyses and reproduce a final figure output
+
+##~~~~~~~~~~~~~~~~~
+##  ~ Authors  ----
+##~~~~~~~~~~~~~~~~~
+# Ava Robillard
+# Contact email: avarobillard@ucsb.edu
+
+
 library(here)
 library(tidyverse)
 library(dplyr)
 library(janitor)
 
-# read in csv files
+rm(list = ls())
+
+# Read in csv files
 PRM <- read_csv(here("data", "RioMameyesPuenteRoto.csv"))
 BQ1 <- read_csv(here("data", "QuebradaCuenca1-Bisley.csv"))
 BQ2 <- read_csv(here("data", "QuebradaCuenca2-Bisley.csv"))
 BQ3 <- read_csv(here("data", "QuebradaCuenca3-Bisley.csv"))
 
-# make really big data frame
+# Make really big data frame
 full_join <- PRM %>% 
   full_join(BQ1) %>% 
   full_join(BQ2) %>% 
   full_join(BQ3) %>% 
   clean_names() 
 
-# make data frame smaller 
+# Make data frame smaller 
 filtered_df <- full_join %>% 
   select("sample_id", "sample_date", "no3_n", "k", "mg", "ca", "nh4_n") %>%
   filter(lubridate::year(sample_date) %in% c(1988:1995)) #%>% 
-  #mutate(day_of_year <- lubridate::yday(sample_date))
+  #mutate(day_of_year <- lubridate::yday(sample_date)) 
 
+# Create a date sequence for one date every 30 days
+dates <- seq(from = as.Date("1989-01-01"), to = as.Date("1994-01-01"), by = 30)
+
+# Function attempts!
 # try to get 9 week averages
 # include in mean + or - 4.5 weeks from given date
 # 4.5 weeks = 31.5 days
-dates <- seq(from = as.Date("1989-01-01"), to = as.Date("1994-01-01"), by = 30)
-means <- c(seq_along(dates))
-get_means <- function(){
-  for (i in seq_along(dates)){
-    last_day <- dates[i] + 32
-    first_day <- dates[i] - 32
-    date_range <- filtered_df %>% 
-      filter(filtered_df$sample_date <= last_day & filtered_df$sample_date >= first_day)
-    mean_k <- mean(date_range$k, na.rm = TRUE)
-    means[i] <- mean_k
+# # create empty vector
+# k_vals <- c()
+# 
+# get_means2 <- function(){
+#   # for each date
+#   for (i in seq_along(dates)){
+#     # for each row in filtered_df
+#     for (j in 1:nrow(filtered_df)){
+#       # get date from df to work with
+#       date <- filtered_df$sample_date[j]
+#       # if the date is in dates between the first and second date (30 days)
+#       if (date %in% c(dates[i]:dates[i+1])){
+#         # the first val in k vector is the same k value as date
+#         k_vals[i] <- filtered_df$k[j]
+#         #also group by site somehow
+#       }
+#     }
+#   # take mean of all k vals
+#   mean_k <- mean(k_vals)
+#   }
+# }
+
+# get_means3 <- function(){
+#   # for each date
+#   for (i in seq_along(dates)){
+#     # for each row in filtered_df
+#     for (j in 1:nrow(filtered_df)){
+#       # get date from df to work with
+#       date <- filtered_df$sample_date[j]
+#       # if the date is in dates between the first and second date (30 days)
+#       if (date %in% c(dates[i]:dates[i+1])){
+#         # the first val in k vector is the same k value as date
+#         k_vals[i] <- filtered_df$k[j]
+#         #also group by site somehow
+#       }
+#     }
+#     # take mean of all k vals
+#     mean_k <- mean(k_vals)
+#   }
+# } 
+
+# need to create the kvals columns
+# filter to correct dates
+
+averages <- c()
+get_average <- function(dataframe) {
+  for (i in seq_along(dates)) {
+    first_day <- dates[i]
+    last_day <- dates[i+1]
+    for (j in 1:nrow(dataframe)) {
+      df_interval <- dataframe %>% 
+      filter(first_day <= dataframe$sample_id[j] & dataframe$sample_id[j] <= last_day)
+      result <- sapply(df_interval$k, mean)
+      averages[i] <- result
+      }
   }
-  return(means)
 }
 
-get_means_attempt2 <- function(){
-  for (i in seq_along(dates)){
-    if filtered_df$sample_date > dates[i+1]:
-     
-  }
-}
-  
+function(filtered_df)
 
-#sapply(mean(dates_found))
 
-# try a pivot longer dataset
+# Try a pivot longer dataset
+  # why- to be able to graph by nutrient
 pivot_for_graph <- filtered_df %>% 
   pivot_longer(cols = c("n03_n", "k", "mg", "ca", "nh4_n"), names_to = nutrient, values_to = mg/L)
 
-# figure out how to facet wrap vertically, and aes(line = site_id) or something to make one plot
+# Figure out how to facet wrap vertically, and aes(line = site_id) or something to make one plot
 # for each nutrient
 # ggplot skeleton
-figurek <- ggplot(filtered_df, aes(x = sample_date #change to averages, y = k)) +
+figurek <- ggplot(filtered_df, aes(x = sample_date, y = k)) +
   geom_line(aes(linetype = sample_id)) 
   #facet_wrap(~k)
 
